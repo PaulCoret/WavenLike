4336239b69ecd5f5617f7dcd45e2133e33c60ef1
PaulCoret <115621137+PaulCoret@users.noreply.github.com>
2022-11-28 17:35:40 +0100

    Highlight

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -13,25 +13,20 @@ public class GridManager : MonoBehaviour
     }
     void GenerateGrid()
     {
+     
         for (int x = 0; x < width; x++)
         {
             for (int z = 0; z < height; z++)
             {
 
-                var spawnedTile = Instantiate(tilePrefab, new Vector3(x, z), Quaternion.identity);
+                var spawnedTile = Instantiate(tilePrefab, new Vector2(x, z), Quaternion.identity);
                 spawnedTile.name = $"Tile {x}, {z}";
 
                 var isOffset = (x % 2 == 0 && z % 2 != 0) || (x % 2 != 0 && z % 2 == 0);
                 spawnedTile.Init(isOffset);
-                    }
 
-            
-
-
-
+              
+            }
         }
-
-
     }
-
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -6,10 +6,20 @@ public class Tile : MonoBehaviour
 {
     [SerializeField] private Material baseColor, offsetColor;
     [SerializeField] private MeshRenderer _renderer;
+    [SerializeField] private GameObject highlight;
 
     public void Init(bool isOffset)
     {
         _renderer.material = isOffset ? offsetColor : baseColor;
-    }   
+    }
+
+    private void OnMouseEnter()
+    {
+        highlight.SetActive(true);
+    }
+    private void OnMouseExit()
+    {
+        highlight.SetActive(false);
+    }
 
 }
